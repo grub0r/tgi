38382434f2ddd3ef34b9511076f6efc5aa730c16
Max Gruber <maxgruber19@gmail.com>
2020-01-04 13:58:23 +0100

    implemented bubble sort

--- tgi01/Sorting.cs
+++ tgi01/Sorting.cs
@@ -1,9 +1,70 @@
-﻿namespace tgi01
+﻿using System;
+
+namespace tgi01
 {
     internal class Sorting
     {
         public static void Main(string[] args)
         {
+
+            int[] toBeSorted = {13, 25, 4, 15, 9, 16, 19};
+            
+            PrintIntegerArray(BubbleSort(toBeSorted));
+
+            Console.ReadLine();
+
+        }
+
+        private static void PrintIntegerArray(int[] input)
+        {
+
+            string output = "[";
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                output = output += input[i].ToString();
+
+                if (i == input.Length - 1)
+                {
+                    output += "";
+                }
+                else
+                {
+                    output += ", ";
+                }
+
+            }
+
+            output += "]";
+            
+            Console.Out.WriteLine(output);
+
+        }
+
+        private static int[] BubbleSort(int[] toBeSorted)
+        {
+
+            for (int i = 0; i < toBeSorted.Length; i++)
+            {
+
+                for (int j = 0; j < toBeSorted.Length - 1; j++)
+                {
+
+                    if (toBeSorted[j] > toBeSorted[j+1])
+                    {
+                        int tmp = toBeSorted[j];
+                        toBeSorted[j] = toBeSorted[j + 1];
+                        toBeSorted[j + 1] = tmp;
+                    }
+                    
+                }
+                
+            }
+
+
+            return toBeSorted;
+
         }
     }
 }
